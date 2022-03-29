4b8dfa6c45daca48504e8f1da3450ce340987e0b
WillyArdiano <willyjessiel99@gmail.com>
2022-03-28 18:48:00 -0600

    Agregando proyectos

---
+++ backend/tiempomaya/src/main/java/com/ts2/tiempomaya/TiempomayaApplication.java
@@ -0,0 +1,13 @@
+package com.ts2.tiempomaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiempomayaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiempomayaApplication.class, args);
+    }
+
+}
---
+++ backend/tiempomaya/src/test/java/com/ts2/tiempomaya/TiempomayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ts2.tiempomaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiempomayaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
