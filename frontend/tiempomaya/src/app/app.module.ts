03e9a416e521cae0a13c290ff3ff62324112a07e
adolfosonchiche <sonadolfo36@gmail.com>
2022-04-16 22:25:08 -0600

    pc

--- frontend/tiempomaya/src/app/app.module.ts
+++ frontend/tiempomaya/src/app/app.module.ts
@@ -4,10 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MemoriaComponent } from './memoria/memoria.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { HomeComponent } from './home/home.component';
+
+// @ts-ignore
 @NgModule({
   declarations: [
     AppComponent,
-    
+    MemoriaComponent,
+    CalculadoraComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
